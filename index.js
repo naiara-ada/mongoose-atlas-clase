0c7a565783453cd27c54eb3ce6e3701df6517309
naiara-ada <naiara.ada@gmail.com>
2024-02-14 11:39:21 +0100

    first up

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const PORT = 8080;
+const {dbConnection} = require('./config/config');
+const routes = require('.routes');
+
+app.use(express.json());
+
+app.use('/', routes)
+
+dbConnection();
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
